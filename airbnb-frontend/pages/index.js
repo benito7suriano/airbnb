2dcec89ab90e2bfd416e71620ef69386d6da8b93
Beno Suriano <bjsuriano@gmail.com>
2022-10-03 22:25:19 -0400

    Get properties from sanity to frontend.

--- airbnb-frontend/pages/index.js
+++ airbnb-frontend/pages/index.js
@@ -1,7 +1,27 @@
 import { sanityClient } from '../sanity'
 
-const Home = () => {
+const Home = ({ properties }) => {
+  console.log(properties)
   return <></>
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[ _type == "property"]'
+  const properties = await sanityClient.fetch(query)
+
+  if (!properties.length) {
+    return {
+      props: {
+        properties: [],
+      },
+    }
+  } else {
+    return {
+      props: {
+        properties,
+      },
+    }
+  }
 }
 
 export default Home

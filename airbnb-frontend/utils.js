ef143e808703a0daeae808e4b862d0ee7b9a78fc
Beno Suriano <bjsuriano@gmail.com>
2022-10-08 14:48:11 -0400

    Make clip title utility function.

--- airbnb-frontend/utils.js
+++ airbnb-frontend/utils.js
@@ -1,1 +1,9 @@
 export const isMultiple = (number) => (number === 0 || number > 1 ? 's' : '')
+
+export const clipTitle = (title) => {
+  if (title.length > 20) {
+    return `${title.substring(0, 20)}...`
+  }
+
+  return title
+}
